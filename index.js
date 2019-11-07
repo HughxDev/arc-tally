a3bdde583e91c51cc7cfce1e9466074b97bcc6a6
Hugh Guiney <hugh@hughguiney.com>
2019-11-07 10:21:41 -0500

    .gitignore

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-var weeklyEarnings =
-  Array.from( document.querySelectorAll( '.roundup__info-block' ) )
-  .filter( node => node.textContent.indexOf( 'Total Earned' ) !== -1 )
-  .map( node => parseFloat( node.textContent.replace( 'Total Earned: $', '' ) ) )
-  .filter( earning => earning > 0 )
-;
-var totalEarnings = weeklyEarnings.reduce( ( total, current ) => total + current );
-var averageEarningsPerWeek = ( totalEarnings / weeklyEarnings.length );
-var averageEarningsPerMonth = ( averageEarningsPerWeek * 4 );
+var invoicedAmounts =
+  Array.from(document.querySelectorAll('.roundup__info-block'))
+    .filter(node => node.textContent.indexOf('Total Earned') !== -1)
+    .map(node => parseFloat(node.textContent.replace('Total Earned: $', '')))
+    .filter(earning => earning > 0)
+  ;
+var totalEarnings = invoicedAmounts.reduce((total, current) => total + current);
 
-console.log( `Weekly Earnings: $${weeklyEarnings.join( ', $' )}` );
-console.log( `Total Weeks: ${weeklyEarnings.length}` );
-console.log( `Total Earnings: $${totalEarnings}` );
-console.log( `Average Earnings per Week: $${averageEarningsPerWeek}` );
-console.log( `Average Earnings per Month: $${averageEarningsPerMonth}` );
+var invoicedHours =
+  Array.from(document.querySelectorAll('.roundup__info-block'))
+    .filter(node => node.textContent.indexOf('Total Spent') !== -1)
+    .map(node => parseFloat(node.textContent.replace(/Total Spent: ([0-9]+(?:\.[0-9]+)?) hours/, '$1')))
+    .filter(hours => hours > 0)
+  ;
+var totalHours = invoicedHours.reduce((total, current) => total + current);
+
+var averageEarningsPerHour = (totalEarnings / totalHours);
+var averageEarningsPerWorkDay = (averageEarningsPerHour * 8);
+var averageEarningsPerInvoice = (totalEarnings / invoicedAmounts.length);
+// Assuming 1 invoice/week:
+var averageEarningsPerMonth = (averageEarningsPerInvoice * 4);
+var averageEarningsPerYear = (averageEarningsPerMonth * 12);
+
+console.log(`Invoiced Amounts: $${invoicedAmounts.join(', $')}`);
+console.log(`Total Hours: ${totalHours}`);
+console.log(`Total Invoices: ${invoicedAmounts.length}`);
+console.log(`Total Earnings: $${totalEarnings}`);
+
+console.log(`Average Earnings per Hour: $${averageEarningsPerWorkDay}`)
+console.log(`Average Earnings per Work Day: $${averageEarningsPerWorkDay}`);
+console.log(`Average Earnings per Invoice: $${averageEarningsPerInvoice}`);
+console.log(`Average Earnings per Month @ 1 invoice/week: $${averageEarningsPerMonth}`);
+console.log(`Average Earnings per Year @ 1 invoice/week: $${averageEarningsPerYear}`);
